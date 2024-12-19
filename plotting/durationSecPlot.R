702dfcb9a499cd98554692169933b1a44e44e444
Simon Liao <50464557+simonliao98@users.noreply.github.com>
2024-12-18 23:49:08 -0500

    Create durationSecPlot.R

---
+++ plotting/durationSecPlot.R
@@ -0,0 +1,23 @@
+plot_duraction_sec <- function(data_list) {
+  print('inside plot_duraction_sec')
+  df <- foreach(i=1:length(data_list), .combine = 'rbind') %do% {
+    data_list[[i]] %>%
+      select(font, targetMeasuredDurationSec,fontNominalSizePt)
+  }
+  if (nrow(df) == 0) {
+    return(NULL)
+  }
+  df <- df %>% mutate(fontNominalSizePt = as.numeric(fontNominalSizePt),
+                      targetMeasuredDurationSec = as.numeric(targetMeasuredDurationSec)) %>%
+    distinct() %>%
+    filter(!is.na(font))
+  p <- ggplot() +
+    geom_point(data=df,
+               aes(x=fontNominalSizePt, 
+                   y = targetMeasuredDurationSec,
+                   color = font)) +
+    scale_x_log10(expand=c(0,0)) +
+    scale_y_log10(expand=c(0,0)) + 
+    labs(title = 'targetMeasuredDurationSec')
+ return(p)
+}
