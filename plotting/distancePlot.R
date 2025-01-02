a2a2c3215d2ee51fa55ad12b8b5bf63e4b4ce3b8
SimonCJLiao <975225926@qq.com>
2025-01-02 08:53:10 -0500

    fix error cause shiny crash

--- plotting/distancePlot.R
+++ plotting/distancePlot.R
@@ -10,7 +10,6 @@ get_measured_distance_data <- function(data_list) {
              !is.na(calibrateTrackDistanceRequestedCm),
              calibrateTrackDistanceMeasuredCm != '',
              calibrateTrackDistanceRequestedCm != '')
-    print(t)
     if (nrow(t) > 0) {
       tmp <- tibble(participant = t$participant[1], 
                     calibrateTrackDistanceMeasuredCm = jsonlite::fromJSON(t$calibrateTrackDistanceMeasuredCm),
@@ -25,9 +24,12 @@ get_measured_distance_data <- function(data_list) {
 plot_distance <- function(data_list) {
   print('inside plot_distance')
   distance <- get_measured_distance_data(data_list)
-  print(distance)
+
+  if (nrow(distance) == 0) {
+    return(NULL)
+  }
   fit <- lm(calibrateTrackDistanceMeasuredCm~calibrateTrackDistanceRequestedCm, data = distance)
-  print(coef(fit))
+
   slope <- coef(fit)
   slope <- format(round(slope[['calibrateTrackDistanceRequestedCm']],2),nsmall=2)
   corr <- cor(distance$calibrateTrackDistanceRequestedCm, distance$calibrateTrackDistanceMeasuredCm)
@@ -35,10 +37,10 @@ plot_distance <- function(data_list) {
   p <- ggplot() + 
     geom_point(data=distance, aes(x = calibrateTrackDistanceRequestedCm, y = calibrateTrackDistanceMeasuredCm)) + 
     ggpp::geom_text_npc(aes(npcx="left",
-                             npcy="top"),
-                         label = paste0('N=',nrow(distance),'\n',
-                                        'R=',corr,'\n',
-                                        'slope=',slope)) + 
+                            npcy="top"),
+                        label = paste0('N=',nrow(distance),'\n',
+                                       'R=',corr,'\n',
+                                       'slope=',slope)) + 
     geom_smooth(method='lm',
                 se=F)
   return(p)
