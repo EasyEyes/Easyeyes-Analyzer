9e8915b3b05c6964387c2d93ab7ab857e68f5f97
SimonCJLiao <975225926@qq.com>
2025-01-29 10:41:54 -0500

    fix error in shiny: remove duplicate elements

--- tabs/timming.R
+++ tabs/timming.R
@@ -13,41 +13,21 @@ timingTab <- tabPanel(
       }
     }
   ")),
-  radioButtons(
-    "fileType",
-    "Select download file type:",
-    c(
-      "eps" = "eps",
-      "pdf" = "pdf",
-      "png" = "png",
-      "svg" = "svg"
-    ),
-    inline = TRUE,
-    selected = "png"
-  ),
-  downloadButton("downloadAll", "Download All"),
-  radioButtons(
-    "filterInput",
-    "Select participants by reading speed:",
-    c(
-      'all' = 'all',
-      'slowest 25%' = 'slowest',
-      'fastest 75%' = 'fastest'
-    ),
-    inline = TRUE,
-    selected = "all"
-  ),
-  fixedRow(
-    div(style = "display: flex; align-items: center; margin-left:12px;",
-        HTML('<div style="font-size: 16px; margin-right: 5px;">Show only spacingDeg thresholds with at least</div>'),
-        numericInput('NQuestTrials', NULL, value = 10, min = 1, width = '80px'),
-        HTML('<div style="font-size: 16px; margin-left: 5px;">trials.</div>')
-    )
-  ),
-  h3("Histograms"),
-  shinycssloaders::withSpinner(uiOutput("timingHistograms"), type = 4),
-  h3("Scatter diagrams"),
+  
   conditionalPanel('output.isDuration', 
+                   h3("Histograms"),
+                   fixedRow(splitLayout(
+                     cellWidths = c("50%", "50%"),
+                     shinycssloaders::withSpinner(imageOutput("durationHist", height = "100%"), type = 4),
+                     shinycssloaders::withSpinner(imageOutput("latenessHist", height = "100%"), type = 4)
+                   )),
+                   fixedRow(splitLayout(
+                     cellWidths = c("50%", "50%"),
+                     downloadButton("downloadDurationHist", "Download"),
+                     downloadButton("downloadLatenessHist", "Download")
+                   )),
+                   shinycssloaders::withSpinner(uiOutput("timingHistograms"), type = 4),
+                   h3("Scatter diagrams"),
                    fixedRow(splitLayout(
                      cellWidths = c("50%", "50%"),
                      shinycssloaders::withSpinner(imageOutput("durationByID", height = '100%'), type = 4),
@@ -68,16 +48,16 @@ timingTab <- tabPanel(
                      downloadButton("downlaodLatenessByID", "Download"),
                      downloadButton("downlaodLatenessByFont", "Download")
                    )),
-  ),
-  fixedRow(splitLayout(
-    cellWidths = c("50%", "50%"),
-    shinycssloaders::withSpinner(imageOutput("durationHist", height = "100%"), type = 4),
-    shinycssloaders::withSpinner(imageOutput("latenessHist", height = "100%"), type = 4)
-  )),
-  fixedRow(splitLayout(
-    cellWidths = c("50%", "50%"),
-    downloadButton("downloadDurationHist", "Download"),
-    downloadButton("downloadLatenessHist", "Download")
-  )),
-  shinycssloaders::withSpinner(uiOutput('scatterTime'),type=4)
+                   fixedRow(splitLayout(
+                     cellWidths = c("50%", "50%"),
+                     shinycssloaders::withSpinner(imageOutput("durationWithFontPadding", height = '100%'), type = 4),
+                     shinycssloaders::withSpinner(imageOutput("latenessWithFontPadding", height = '100%'), type = 4)
+                   )),
+                   fixedRow(splitLayout(
+                     cellWidths = c("50%", "50%"),
+                     downloadButton("downlaodDurationWithFontPadding", "Download"),
+                     downloadButton("downlaodLatenessWithFontPadding", "Download")
+                   )),
+                   shinycssloaders::withSpinner(uiOutput('scatterTime'),type=4)
+  )
 )
